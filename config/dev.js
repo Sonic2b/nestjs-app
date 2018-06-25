a2b5bd8d5fd4493aec526735f1d6279729fe42f7
leonzhao <yimingdream@gmail.com>
2018-06-25 10:53:53 +0800

    update(redis) config

--- config/dev.js
+++ config/dev.js
@@ -1,6 +1,9 @@
 module.exports = {
   env: process.env.NODE_ENV,
   redis: {
-    url: 'redis://localhost:27017'
+    url: 'redis://192.168.0.45:6379'
+  },
+  mongodb: {
+    url: 'mongodb://192.168.0.25:27017'
   }
 }
